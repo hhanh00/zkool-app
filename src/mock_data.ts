ea3f7136bd15b6d8950a8619f57dc90dd88c5975
hhanh00 <hanh425@gmail.com>
2025-01-26 20:17:14 +1000

    feat: account list (#5)

---
+++ src/mock_data.ts
@@ -0,0 +1,34 @@
+import { Account } from "./types";
+
+export const mock_accounts: Account[] = [
+  {
+    id: 1,
+    name: "Main",
+    icon: "data",
+    order: 3,
+    description: "Main Account",
+    balance: 40000000,
+    birth: 2100000,
+    height: 2200000,
+  },
+  {
+    id: 2,
+    name: "Second",
+    icon: "data",
+    order: 1,
+    description: "",
+    balance: 80000000,
+    birth: 2100000,
+    height: 2200000,
+  },
+  {
+    id: 3,
+    name: "Pocket",
+    icon: "data",
+    order: 2,
+    description: "Change",
+    balance: 120000000,
+    birth: 2100000,
+    height: 2200000,
+  },
+]
