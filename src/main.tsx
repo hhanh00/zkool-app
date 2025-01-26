dddb68f4cb514e5c0504caafcb4b999300555c95
hhanh00 <hanh425@gmail.com>
2025-01-26 22:47:01 +1000

    feat: new account (#7)

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import BottomBar from './components/ui/bottom_nav.tsx'
-import { Open } from './routes/Open.tsx'
-import { AccountList } from './routes/AccountManager.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Open } from "@/routes/Open.tsx";
+import { NewAccount } from "@/routes/NewAccount.tsx";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <BottomBar />
       <Routes>
-      <Route path="/" element={<AccountList />} />
-      <Route path="/open" element={<Open />} />
+        <Route path="/" element={<NewAccount />} />
+        <Route path="/open" element={<Open />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
