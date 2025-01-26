b2d24e7988b989ac3d24e8f4b02a2be30dddce21
Hanh <hanh425@gmail.com>
2025-01-26 20:58:59 +1000

    add blank form

--- src/routes/NewAccount.tsx
+++ src/routes/NewAccount.tsx
@@ -1,3 +1,35 @@
+import { Form } from "@/components/ui/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  birth: z.number(),
+  key: z.string(),
+  index: z.number(),
+  transparent: z.boolean(),
+  sapling: z.boolean(),
+  orchard: z.boolean(),
+  use_any_address_index: z.boolean(),
+})
+
+
 export const NewAccount: React.FC<{}> = () => {
-  return <div />
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      index: 0,
+      use_any_address_index: false,
+    },
+  })
+
+  return (
+    <Form {...form}>
+      <form></form>
+    </Form>
+  )
 }
